7512c49a8b94d21618191c662c59713fec599821
begato <eslam.rali@gmail.com>
2022-06-28 18:46:46 +0200

    change moduel size

--- lib/counter_moduel.dart
+++ lib/counter_moduel.dart
@@ -8,8 +8,8 @@ class CounterModuel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 151,
-      height: 151,
+      width: 152,
+      height: 152,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: Colors.white,
